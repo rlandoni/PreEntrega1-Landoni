538ff108ffd1f95bac6a0240a3ba620d10bcd56e
Rodrigo Landoni <rodrilandoni1995@gmail.com>
2023-01-31 00:08:37 +0100

    arreglo cantidad de bucles

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 // BIENVENIDA
 let nombre = prompt("Ingrese el nombre del alumno: ")
-alert("Bienvenido " + nombre) + "vamos a calcular tu promedio."
-alert("Puedes insertar -Exit- para salir")
+alert("Bienvenido " + nombre + "vamos a calcular tu promedio.");
 
-function calculo(alumno) {
-    while (alumno != "Exit") {
-        let notaA = parseInt(prompt("Ingrese a continuacion la primera nota"))
-        let notaB = parseInt(prompt("Ahora la segunda nota"))
-        let promedio = (notaA + notaB) / 2
-        if (promedio < 4) {
-            alert(nombre + " has desaprobado con " + promedio)
+for (let i = 0; i < 50; i++) {
+
+    // CALCULO
+    function calculo(alumno) {
+        while (alumno != "Exit") {
+            let notaA = parseInt(prompt("Ingrese a continuacion la primera nota"))
+            let notaB = parseInt(prompt("Ahora la segunda nota"))
+            let promedio = (notaA + notaB) / 2
+            if (promedio < 4) {
+                alert(nombre + " has desaprobado con un promedio de " + promedio)
+                break;
+            }
+            else if (promedio == 4) {
+                alert(nombre + " aprobaste raspando con un promedio de " + promedio);
+                break;
+            }
+            else (promedio > 4);
+            alert(nombre + " aprobaste, FELICITACIONES! Tu promedio fue de " + promedio)
             break;
         }
-        else if (promedio = 4) {
-            alert(nombre + " aprobaste raspando!");
-            break;
-        }
-        else (promedio > 4);
-        alert(nombre + " aprobaste, FELICITACIONES!")
-        break;
     }
+
+    alumno = prompt("Ingrese el nombre del alumno: ")
+
+    calculo(alumno);
+
+
 }
-
-
-
-alumno = prompt("Ingrese el nombre del alumno: ")
-
-calculo(alumno);
-
